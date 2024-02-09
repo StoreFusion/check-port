3438ad3a053df26bb939f3ac51d3c41e2afde3e7
StoreFusionMain <tdur@protonmail.com>
2024-02-09 15:33:46 +0530

    [minor[ txt changed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -41,14 +41,11 @@ const AboutPage = () => {
             <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
             {/* BIOGRAPHY DESC */}
             <p className="text-lg">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              harum quibusdam cupiditate nobis accusamus sed aut aperiam,
-              reiciendis numquam! Voluptas voluptatibus obcaecati dolore itaque
-              suscipit! Vel doloremque numquam quam nihil.
+              Filler desc.
             </p>
             {/* BIOGRAPHY QUOTE */}
             <span className="italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Filler content.
             </span>
             {/* BIOGRAPHY SIGN SVG*/}
             {/* <div className="self-end">
